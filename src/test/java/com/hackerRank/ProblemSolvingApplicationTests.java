bd0b419eb4da14ce6fc3f25c28b5ae0c82c6737a
edgar <edgarlopez.hdez@gmail.com>
2023-12-02 14:27:00 -0600

    Initial commit base project from spring initializer

---
+++ src/test/java/com/hackerRank/ProblemSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
